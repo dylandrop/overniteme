5ef4191b176e075cce4f225c8d442c1cd40dc203
Dylan <dylandrop@gmail.com>
2012-08-18 13:07:24 -0400

    added route and link to start new conversation)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Overniteme::Application.routes.draw do
   devise_for :users
+  resources :conversations, :only => [:new, :create, :show]
   resources :users, :only => [:index, :show]
   root :to => 'static#home'
 end
