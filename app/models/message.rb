fb6636925a8788b0c0e21f4fb2916ce15ef38384
Dylan <dylandrop@gmail.com>
2012-08-21 16:03:38 -0700

    added validation for creating a reply

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,12 @@ class Message < ActiveRecord::Base
   belongs_to :from, class_name: "User", foreign_key: 'from_id'
   belongs_to :conversation
   attr_accessible :from, :to, :subject, :content, :conversation_id
+  validate :can_be_sent_to_recipient
+
+  private
+  def can_be_sent_to_recipient
+    if conversation.nil?
+      errors.add(:base, "Something went wrong.")
+    end
+  end 
 end
