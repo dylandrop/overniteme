96865869138a953db0fd55461399c4d5deba0a79
Dylan <dylandrop@gmail.com>
2012-08-22 10:24:17 -0700

    fixed migrations

--- db/migrate/20120802224846_replace_sexuality_with_seeking_for_users.rb
+++ db/migrate/20120802224846_replace_sexuality_with_seeking_for_users.rb
@@ -1,11 +1,11 @@
 class ReplaceSexualityWithSeekingForUsers < ActiveRecord::Migration
   def up
     add_column :users, :seeking, :string
-    remove_column :users, :sexuality, :string
+    remove_column :users, :sexuality
   end
 
   def down
     add_column :users, :sexuality, :string
-    add_column :users, :seeking, :string
+    remove_column :users, :seeking
   end
 end
--- db/migrate/20120820215623_remove_to_id_from_messages.rb
+++ db/migrate/20120820215623_remove_to_id_from_messages.rb
@@ -1,6 +1,6 @@
 class RemoveToIdFromMessages < ActiveRecord::Migration
   def up
-    remove_column :messages, :to_id, :integer
+    remove_column :messages, :to_id
   end
 
   def down
