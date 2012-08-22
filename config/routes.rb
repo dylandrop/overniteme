2f9a17c077a0b1def16e8e7bf47a8ada0abe4e8f
Dylan <dylandrop@gmail.com>
2012-08-21 19:59:04 -0700

    fixed nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Overniteme::Application.routes.draw do
   devise_for :users
+  match "contact" => "static#contact"
   resources :conversations, :only => [:new, :create, :show, :index]
   resources :messages, :only => [:create]
   resources :users, :only => [:index, :show]
